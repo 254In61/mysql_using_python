f6bab1b2dcbf958593f9e7e3f2a07e8bcad11ce2
awsso-amaseghe <amaseghe@datafied.com>
2022-06-29 22:37:21 +1000

    Cleanup

--- ProjectModules/common_vars.py
+++
@@ -1,4 +0,0 @@
-# Variables shared by both Client and Server
-
-HOST = '192.168.1.111' # Server's IP address..Can be resolved using gethostbyname() if using DNS.
-PORT = 50055  # Arbitrary non-privileged port
--- common_vars.py
+++
@@ -1,3 +0,0 @@
-# Common variables
-HOST = '192.168.1.111' # Server's IP address..Can be resolved using gethostbyname() if using DNS.
-PORT = 50055   # Arbitrary non-privileged port
